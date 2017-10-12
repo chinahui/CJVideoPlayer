810bc2c76c3971264b09807621e7093b842c4efa
chinahui <huikey@foxmail.com>
2017-10-12 11:05:20 +0800

    init

---
+++ CJVideoPlayer/CJVideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJVideoPlayer
+//
+//  Created by Cray Joy on 2017/10/12.
+//  Copyright © 2017年 HuiKey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CJVideoPlayer/CJVideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CJVideoPlayer
+//
+//  Created by Cray Joy on 2017/10/12.
+//  Copyright © 2017年 HuiKey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
